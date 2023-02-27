a15c4ea78ced036994318cd880b98661ff994e24
Isaak Dyck <isaakddyck@hotmail.com>
2023-02-27 11:06:15 -0600

    added styling to the 6 random numbers

--- src/components/game.js
+++ src/components/game.js
@@ -9,17 +9,21 @@ class Game extends React.Component {
     };
     randomNumbers = Array
         .from({length: this.props.randomNumCount})
-        .map(() => 1 + Math.floor(10 * Math.random()));
+        .map(() => 1 + Math.floor(15 * Math.random()));
     target = this.randomNumbers
     .slice(0, this.props.randomNumCount - 2)
     .reduce((acc, curr) => acc + curr, 0);
+    // TODO: Shuffle the random numbers
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.target}>{this.target}</Text>
-                {this.randomNumbers.map((randomNumber, index) =>
-                    <Text key={index}>{randomNumber}</Text>
-                )}
+                <View style={styles.randomContainer}>
+                    {this.randomNumbers.map((randomNumber, index) =>
+                        <Text style={styles.random} key={index}>{randomNumber}</Text>
+                    )}
+                </View>
             </View>
         );
     }
@@ -33,9 +37,25 @@ const styles = StyleSheet.create({
     },
 
     target: {
-    fontSize: 40,
+    fontSize: 50,
     backgroundColor: '#aaa',
-    marginHorizontal: 50,
+    margin: 50,
+    textAlign: 'center',
+    },
+
+    randomContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    },
+
+    random: {
+    backgroundColor: '#999',
+    width: 100,
+    marginHorizontal: 30,
+    marginVertical: 40,
+    fontSize: 35,
     textAlign: 'center',
     },
 });
